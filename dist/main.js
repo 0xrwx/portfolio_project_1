1a9d390b3a6407af8ff4b67b627f3c44b2bb6040
1Sayd <person.tak@gmail.com>
2022-08-11 17:04:35 +0300

    change config

---
+++ dist/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack://portfolio_project_1/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist/'),
     clean: true
-  }
+  },
+  module: {
+    rules: [
+
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin( {
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: false
+  } )
+],
 }
