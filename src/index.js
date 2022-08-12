f1bc819125ac114d5be411e10a6d4d38f8cc4654
1Sayd <person.tak@gmail.com>
2022-08-12 15:40:06 +0300

    build

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import doForm from './js/script.js';
-import './css/normalize.css';
-import './css/soft-reset.css';
-import './scss/style.scss'
-
-const form = document.forms.contact;
-form.addEventListener("submit", doForm);
